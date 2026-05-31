e58472fc0d9d58115389f947b7fd7cab064102a7
Vikhyat Sharma <vikhyats93@gmail.com>
2026-05-31 12:27:16 +0530

    chore: calling pollUpdates

--- TelegramManager.h
+++ TelegramManager.h
@@ -9,6 +9,8 @@ class TelegramManager {
 public:
   void sendMessage(String message, String chatId = TELEGRAM_CHAT_ID);
   void handleCommand(const String& fromChatID, const String& messageText);
+  // Poll for new updates from Telegram and dispatch commands
+  void pollUpdates();
   bool removeApprovedChatID(const String& chatID);
   void sendToApproved(String message);
   void saveApprovedChatID(const String& chatID);
